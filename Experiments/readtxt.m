27059af9db2de9a16e816a0e4b7270e35fdc9269
Ekaterina Kopets <kopets.ekaterina@gmail.com>
2022-06-06 20:46:01 +0300

    Update readtxt.m

--- Experiments/readtxt.m
+++ Experiments/readtxt.m
@@ -1,11 +1,11 @@
-fileID = '28_04.TXT';
+fileID = 'TEST8.TXT';
 L = 9;
 T = readtable(fileID);
 Data = table2array(T);
 time = Data(:,1);
+repiods =[0, 50000, 100000, 150000, 200000, 250000, 300000, 350000];
 figure(1); hold on
-for i = [2,9]
-    
+for i =2:2
     %figure;
     plot(time,Data(:,i),'-','Linewidth',1);
     ylim([1.7, 2.45])
